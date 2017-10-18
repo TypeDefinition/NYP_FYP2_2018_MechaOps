ac83db13b4d6047f5c4b5466715373e4f05f1832
Terry Lim <lnxterry@gmail.com>
2017-10-18 21:45:42 +0800

    Completed Tile Generation System. Added functionality to modify tile type in editor.

---
+++ MechaOps/Assets/Editor/TileEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(Tile))]
+[CanEditMultipleObjects]
+public class TileEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        DrawDefaultInspector();
+
+        Tile tile = (Tile)target;
+        if (GUILayout.Button("Load Type")) {
+            tile.LoadType();
+        }
+    }
+
+}
