29a91ba1ec90cf719c72a941e3b1a74c11057184
Ah Heng <leesekheng@yahoo.com>
2017-12-09 15:23:39 +0800

    Tweened the UI scale for most of the UI

---
+++ MechaOps/Assets/Scripts/Unit/Unit Logic/EnemyViewTile.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This will be used to render this unit invisible! inherit from PlayerViewScript to be lazy
+/// </summary>
+public class EnemyViewTile : ViewTileScript
+{
+    [Header("Debugging for EnemyViewTile")]
+    [SerializeField, Tooltip("Enemy Visiblity counter. Do not touch")]
+    protected int m_VisibilityCount = 0;
+    [SerializeField, Tooltip("The array of MeshRenderer inside this unit")]
+    protected MeshRenderer[] m_AllRenderers;
+
+    public int VisibilityCount
+    {
+        set
+        {
+            m_VisibilityCount = Mathf.Max(0, value);
+            switch (m_VisibilityCount)
+            {
+                case 0:
+                    // if the render is active to begin with, make everything else inactive including itself!
+                    if (m_AllRenderers[0].enabled)
+                    {
+                        foreach (MeshRenderer zeRender in m_AllRenderers)
+                        {
+                            zeRender.enabled = false;
+                        }
+                    }
+                    break;
+                default:
+                    // if the render is inactive, make everything else active including itself!
+                    if (!m_AllRenderers[0].enabled)
+                    {
+                        foreach (MeshRenderer zeRender in m_AllRenderers)
+                        {
+                            zeRender.enabled = true;
+                        }
+                    }
+                    break;
+            }
+        }
+        get
+        {
+            return m_VisibilityCount;
+        }
+    }
+
+    protected void Awake()
+    {
+        m_AllRenderers = GetComponentsInChildren<MeshRenderer>();
+    }
+
+    public override void DecreVisi()
+    {
+        --VisibilityCount;
+    }
+
+    public override void IncreVisi()
+    {
+        ++VisibilityCount;
+    }
+
+    /// <summary>
+    /// Need this to check whether it renders itself or not!
+    /// </summary>
+    public override void RenderSurroundingTiles()
+    {
+        VisibilityCount = VisibilityCount;
+    }
+}
