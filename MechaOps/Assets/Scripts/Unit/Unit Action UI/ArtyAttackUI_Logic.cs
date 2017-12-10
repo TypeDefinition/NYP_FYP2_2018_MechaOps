a94adb3723b10311fececcd5102f87689aa5cc7c
Ah Heng <leesekheng@yahoo.com>
2017-12-10 13:24:55 +0800

    Making the artillery attack action in progress

--- MechaOps/Assets/Scripts/Unit/Unit Action UI/ArtyAttackUI_Logic.cs
+++ MechaOps/Assets/Scripts/Unit/Unit Action UI/ArtyAttackUI_Logic.cs
@@ -2,10 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ArtyAttackUI_Logic : MonoBehaviour {
+/// <summary>
+/// The logic for artillery attack UI
+/// </summary>
+public class ArtyAttackUI_Logic : TweenUI_Scale {
+    [Header("Debugging for ArtyAttackUI")]
+    [SerializeField, Tooltip("Unit's attack action")]
+    protected UnitAttackAction m_AttckAct;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private void OnEnable()
+    {
+        // Animate the UI when enabled
+        AnimateUI();
+
+        GameEventSystem.GetInstance().TriggerEvent("ToggleSelectingUnit");
+        GameEventSystem.GetInstance().SubscribeToEvent<IUnitAction>("SelectedAction", PressedAction);
+    }
+
+    private void OnDisable()
+    {
+        
+    }
+
+
+
+    /// <summary>
+    /// When pressing the back button
+    /// </summary>
+    public void Back()
+    {
+        // ensure that the player will be able to click on unit again!
+        GameEventSystem.GetInstance().TriggerEvent("ToggleSelectingUnit");
+        // there is no point in keeping this UI anymore so destroy it!
+        Destroy(gameObject);
+    }
+
+    public void ConfirmAttacl()
+    {
+        Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// To get the event message from the GameEventSystem
+    /// </summary>
+    /// <param name="_act"></param>
+    public void PressedAction(IUnitAction _act)
+    {
+        m_AttckAct = _act as UnitAttackAction;
+    }
 }
---
+++ MechaOps/Assets/Scripts/Unit/Unit Logic/Unit Actions/Artillery/ArtyAttackAct.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// For the artillery unit attack logic
+/// </summary>
+public class ArtyAttackAct : UnitAttackAction {
+
+}
