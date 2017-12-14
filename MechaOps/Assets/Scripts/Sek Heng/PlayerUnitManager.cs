5874db6a26221a9ce28865d82bd02aa06aeca4e3
Terry Lim <lnxterry@gmail.com>
2017-12-15 01:16:50 +0800

    Renamed a most of the variables in PlayerUnitManager.

--- MechaOps/Assets/Scripts/Sek Heng/PlayerUnitManager.cs
+++ MechaOps/Assets/Scripts/Sek Heng/PlayerUnitManager.cs
@@ -7,59 +7,74 @@ using TMPro;
 public class PlayerUnitManager : MonoBehaviour
 {
     [Header("Linking and variables required")]
-    public GetPlayerInputUnit m_PlayerInputOnUnit;
-    [Tooltip("The prefab holder for the unit's actions!")]
-    public Image m_UnitActionUIIconGO;
-    [Tooltip("The ScrollRect of the unit icons")]
-    public GameObject m_ScrollRectUnitActionSelection;
-    [Tooltip("The parent to contain all of these icons")]
-    public GameObject m_ScrollRectContent;
-    [Tooltip("The canvas transform that holds all of the action stuff")]
-    public Transform m_UICanvasTransform;
-    [Tooltip("UI Stats display script")]
-    public UnitDisplayUI m_UnitDisplayUI;
+
+    // Prefabs used for copy initialisation.
+    [SerializeField, Tooltip("UI Stats display script")]
+    private UnitDisplayUI m_UnitDisplayUI_Prefab;
+    [SerializeField, Tooltip("The prefab holder for the unit's actions!")]
+    private Image m_UnitActionIconImage_Prefab;
+
+    // GetPlayerInputUnit checks for which unit is clicked on.
+    [SerializeField]
+    private GetPlayerInputUnit m_GetPlayerInputUnit;
+
+    // KeepTrackOfUnits keeps tracks of units that are still alive.
+    [SerializeField, Tooltip("Keep Track of Unit Script")]
+    private KeepTrackOfUnits m_KeepTrackOfUnits;
+
+    // World Canvas
+    [SerializeField, Tooltip("The world canvas transform")]
+    private Transform m_WorldCanvasTransform;
+
+    // Objects in the Player Control Canvas.
+    [SerializeField, Tooltip("The ScrollRect of the unit icons")]
+    private GameObject m_ScrollRectUnitActionSelection;
+    [SerializeField, Tooltip("The parent to contain all of these icons")]
+    private GameObject m_ScrollRectContent;
+    [SerializeField, Tooltip("The canvas transform that holds all of the action stuff")]
+    private Transform m_UICanvasTransform;
     [SerializeField, Tooltip("The gameobject to toggle between selecting units")]
-    protected GameObject m_UnitSelection;
+    private GameObject m_UnitSelectionUI;
     [SerializeField, Tooltip("The text UI to display the unit's name!")]
-    protected TextMeshProUGUI m_UnitNameTextUI;
+    private TextMeshProUGUI m_UnitNameTextUI;
 
     [Header("Debugging References")]
     [SerializeField, Tooltip("The array of how many units have yet to make their turn. Meant for debugging purpose")]
-    protected List<GameObject> m_UnitsYetToMakeMoves;
-    [Tooltip("The number of image icons beneath it")]
-    public List<Button> m_AllOfUnitUIIcon;
-    [Tooltip("The current unit action that is clicked upon")]
-    public IUnitAction m_CurrentSelectedAction;
-    [Tooltip("The player unit that has been clicked upon")]
-    public GameObject m_SelectedPlayerUnit;
-    [SerializeField, Tooltip("The world canvas transform")]
-    protected Transform m_WorldCanvasTrans;
-    [SerializeField, Tooltip("The instantiated unit display UI")]
-    protected UnitDisplayUI m_InstantiatedUnitUI;
+    private List<GameObject> m_UnitsYetToMakeMoves;
     [SerializeField, Tooltip("The index of the current selected unit for select between units")]
-    protected int m_SelectedUnitIndex = 0;
-    [SerializeField, Tooltip("Keep Track of Unit Script")]
-    protected KeepTrackOfUnits m_TrackedUnits;
+    private int m_SelectedUnitIndex = 0;
+    [SerializeField, Tooltip("The player unit that has been clicked upon")]
+    private GameObject m_SelectedPlayerUnit;
+    [SerializeField, Tooltip("The number of image icons beneath it")]
+    private List<Button> m_SelectedUnitActionButtons;
+    [SerializeField, Tooltip("The current unit action that is clicked upon")]
+    public IUnitAction m_CurrentSelectedAction;
+    [SerializeField, Tooltip("The instantiated unit display UI")]
+    private UnitDisplayUI m_UnitDisplayUI;
 
     /// <summary>
-    /// The update of this manager. So that it can be controlled anytime
+    /// The update of this manager. So that it can be controlled anytime.
     /// </summary>
-    protected Coroutine m_UpdateOfManager;
+    protected Coroutine m_UpdateOfManager; // Does this even do anything? -Terry
 
     private void Start()
     {
-        if (!m_TrackedUnits)
-            m_TrackedUnits = GetComponent<KeepTrackOfUnits>();
-        if (!m_WorldCanvasTrans)
-            m_WorldCanvasTrans = GameObject.FindGameObjectWithTag("WorldCanvas").transform;
-        if (!m_InstantiatedUnitUI)
+        if (!m_KeepTrackOfUnits)
         {
-            m_InstantiatedUnitUI = Instantiate(m_UnitDisplayUI.gameObject, m_WorldCanvasTrans).GetComponent<UnitDisplayUI>();
-            m_InstantiatedUnitUI.gameObject.SetActive(false);
+            m_KeepTrackOfUnits = GetComponent<KeepTrackOfUnits>();
+        }
+        if (!m_WorldCanvasTransform)
+        {
+            m_WorldCanvasTransform = GameObject.FindGameObjectWithTag("WorldCanvas").transform;
+        }
+        if (!m_UnitDisplayUI)
+        {
+            m_UnitDisplayUI = Instantiate(m_UnitDisplayUI_Prefab.gameObject, m_WorldCanvasTransform).GetComponent<UnitDisplayUI>();
+            m_UnitDisplayUI.gameObject.SetActive(false);
         }
     }
 
-    private void OnEnable()
+    private void InitEvents()
     {
         GameEventSystem.GetInstance().SubscribeToEvent("PlayerAnnihilated", StopUpdate);
         GameEventSystem.GetInstance().SubscribeToEvent("EnemyAnnihilated", StopUpdate);
@@ -68,7 +83,7 @@ public class PlayerUnitManager : MonoBehaviour
         GameEventSystem.GetInstance().SubscribeToEvent("UnitFinishAction", PollingForPlayerInput);
     }
 
-    private void OnDisable()
+    private void DeinitEvents()
     {
         GameEventSystem.GetInstance().UnsubscribeFromEvent("PlayerAnnihilated", StopUpdate);
         GameEventSystem.GetInstance().UnsubscribeFromEvent("EnemyAnnihilated", StopUpdate);
@@ -77,27 +92,46 @@ public class PlayerUnitManager : MonoBehaviour
         GameEventSystem.GetInstance().UnsubscribeFromEvent("UnitFinishAction", PollingForPlayerInput);
     }
 
+    private void OnEnable()
+    {
+        InitEvents();
+    }
+
+    private void OnDisable()
+    {
+        DeinitEvents();
+    }
+
     public IEnumerator BeginUpdateOfPlayerUnits()
     {
-        m_UnitSelection.SetActive(true);
-        m_SelectedUnitIndex = 0;
         // Get a shallow copy of the list of all available units!
-        m_UnitsYetToMakeMoves = new List<GameObject>(m_TrackedUnits.m_AllPlayerUnitGO);
+        m_UnitsYetToMakeMoves = new List<GameObject>(m_KeepTrackOfUnits.m_AllPlayerUnitGO);
+        m_SelectedUnitIndex = 0;
+        m_UnitSelectionUI.SetActive(true);
+
+        // Subscribe to Events.
         GameEventSystem.GetInstance().SubscribeToEvent("UnitFinishAction", PollingForPlayerInput);
         GameEventSystem.GetInstance().SubscribeToEvent<GameObject>("UnitMakeMove", UnitHasMakeMove);
-        WaitForSecondsRealtime zeAmountOfWaitTime = new WaitForSecondsRealtime(0.1f);
-        m_PlayerInputOnUnit.enabled = true;
+
+        // Wait for all units to make their move.
+        m_GetPlayerInputUnit.enabled = true;
+        WaitForSecondsRealtime waitForSecondsRealtime = new WaitForSecondsRealtime(0.1f);
         while (m_UnitsYetToMakeMoves.Count > 0)
         {
-            yield return zeAmountOfWaitTime;
+            yield return waitForSecondsRealtime;
         }
+
+        // Now that all units have completed their moves, unsubscribe from the events.
         GameEventSystem.GetInstance().UnsubscribeFromEvent<GameObject>("UnitMakeMove", UnitHasMakeMove);
         GameEventSystem.GetInstance().UnsubscribeFromEvent("UnitFinishAction", PollingForPlayerInput);
-        m_UpdateOfManager = null;
+
+        m_UpdateOfManager = null; // Is this even used?
+        
+        // Player no longer needs to interact with the game so might as well turn off the polling.
+        m_GetPlayerInputUnit.enabled = false;
+        m_UnitSelectionUI.SetActive(false);
+
         GameEventSystem.GetInstance().TriggerEvent("TurnEnded");
-        // Player no longer needs to interact with the game so might as well turn off the polling
-        m_PlayerInputOnUnit.enabled = false;
-        m_UnitSelection.SetActive(false);
         yield break;
     }
 	
@@ -106,9 +140,12 @@ public class PlayerUnitManager : MonoBehaviour
     /// </summary>
     protected void StopUpdate()
     {
+        // Unsubscribe from events.
         GameEventSystem.GetInstance().UnsubscribeFromEvent<GameObject>("UnitMakeMove", UnitHasMakeMove);
         GameEventSystem.GetInstance().UnsubscribeFromEvent<GameObject>("ClickedUnit", PlayerSelectUnit);
         GameEventSystem.GetInstance().UnsubscribeFromEvent("UnitFinishAction", PollingForPlayerInput);
+
+        // Once again, is this even used?
         if (m_UpdateOfManager != null)
         {
             StopCoroutine(m_UpdateOfManager);
@@ -124,15 +161,14 @@ public class PlayerUnitManager : MonoBehaviour
         m_UnitsYetToMakeMoves.Remove(_unitGoFinished);
     }
 
-    protected void PlayerSelectUnit(GameObject _UnitGO)
+    protected void PlayerSelectUnit(GameObject _unit)
     {
         // If only the clicked unit belongs to the player and it must be inside the list of player unit that has yet to make a move!
-        if (_UnitGO.tag == "Player" && m_UnitsYetToMakeMoves.Contains(_UnitGO))
+        if (_unit.tag == "Player" && m_UnitsYetToMakeMoves.Contains(_unit))
         {
-            SpawnActionUI(_UnitGO);
+            SpawnActionUI(_unit);
         }
     }
-
 
     /// <summary>
     /// Helps to toggle the observer action subscription and the UI too!
@@ -140,17 +176,18 @@ public class PlayerUnitManager : MonoBehaviour
     protected void ToggleThePlayerInput()
     {
         m_ScrollRectUnitActionSelection.SetActive(!m_ScrollRectUnitActionSelection.activeSelf);
-        switch (m_ScrollRectUnitActionSelection.activeSelf)
+        if (m_ScrollRectUnitActionSelection.activeSelf)
         {
-            case true:
-                m_InstantiatedUnitUI.gameObject.SetActive(true);
-                m_UnitSelection.SetActive(true);
-                GameEventSystem.GetInstance().SubscribeToEvent<GameObject>("ClickedUnit", PlayerSelectUnit);
-                break;
-            default:
-                m_InstantiatedUnitUI.gameObject.SetActive(false);
-                GameEventSystem.GetInstance().UnsubscribeFromEvent<GameObject>("ClickedUnit", PlayerSelectUnit);
-                break;
+            m_UnitDisplayUI.gameObject.SetActive(true);
+            m_UnitSelectionUI.SetActive(true);
+
+            GameEventSystem.GetInstance().SubscribeToEvent<GameObject>("ClickedUnit", PlayerSelectUnit);
+        }
+        else
+        {
+            m_UnitDisplayUI.gameObject.SetActive(false);
+
+            GameEventSystem.GetInstance().UnsubscribeFromEvent<GameObject>("ClickedUnit", PlayerSelectUnit);
         }
     }
 
@@ -159,7 +196,7 @@ public class PlayerUnitManager : MonoBehaviour
     /// </summary>
     protected void PollingForPlayerInput()
     {
-        m_UnitSelection.SetActive(true);
+        m_UnitSelectionUI.SetActive(true);
         GameEventSystem.GetInstance().SubscribeToEvent<GameObject>("ClickedUnit", PlayerSelectUnit);
     }
 
@@ -170,8 +207,8 @@ public class PlayerUnitManager : MonoBehaviour
     /// <param name="_SelectedAction">The reference to that UI</param>
     public void ToInstantiateSpecificActionUI(GameObject _SpecificUIGO, IUnitAction _SelectedAction)
     {
-        m_InstantiatedUnitUI.gameObject.SetActive(false);
-        m_UnitSelection.SetActive(false);
+        m_UnitDisplayUI.gameObject.SetActive(false);
+        m_UnitSelectionUI.SetActive(false);
         m_CurrentSelectedAction = _SelectedAction;
         Instantiate(_SpecificUIGO, m_UICanvasTransform).SetActive(true);
         GameEventSystem.GetInstance().TriggerEvent<IUnitAction>("SelectedAction", m_CurrentSelectedAction);
@@ -217,37 +254,50 @@ public class PlayerUnitManager : MonoBehaviour
     /// <summary>
     /// It will help to spawn those action UI icons
     /// </summary>
-    /// <param name="_go">The unit gameobject which must have the unit stats and unit action for sure!</param>
-    protected void SpawnActionUI(GameObject _go)
+    /// <param name="_unit">The unit gameobject which must have the unit stats and unit action for sure!</param>
+    protected void SpawnActionUI(GameObject _unit)
     {
-        m_UnitNameTextUI.text = _go.name;
+        m_SelectedPlayerUnit = _unit;
+
+        m_UnitNameTextUI.text = _unit.name;
         m_ScrollRectUnitActionSelection.SetActive(true);
-        // Need to ensure the selectedPlayerUnit is thr
-        m_SelectedPlayerUnit = _go;
-        IUnitAction[] allPossibleUnitActions = _go.GetComponentsInChildren<IUnitAction>();
-        if (!m_InstantiatedUnitUI.gameObject.activeSelf)
-            m_InstantiatedUnitUI.gameObject.SetActive(true);
-        else
-            m_InstantiatedUnitUI.AnimateUI();
-        m_InstantiatedUnitUI.SetThePosToUnit(_go.GetComponent<UnitStats>());
-        #region Clear The old buttons
-        foreach (Button zeUnitButton in m_AllOfUnitUIIcon)
+
+        if (!m_UnitDisplayUI.gameObject.activeSelf)
         {
-            Destroy(zeUnitButton.gameObject);
+            m_UnitDisplayUI.gameObject.SetActive(true);
         }
-        m_AllOfUnitUIIcon.Clear();
-        #endregion
-        // When we will instantiate the button according to the amount of unit actions there are!
-        foreach (IUnitAction zeUnitAction in allPossibleUnitActions)
+        else
         {
-            Image zeUnitIconUI = Instantiate(m_UnitActionUIIconGO, m_ScrollRectContent.transform);
-            // Replace the sprite
-            zeUnitIconUI.gameObject.SetActive(true);
-            zeUnitIconUI.sprite = zeUnitAction.ActionIconUI;
+            m_UnitDisplayUI.AnimateUI();
+        }
+        m_UnitDisplayUI.SetThePosToUnit(_unit.GetComponent<UnitStats>());
+        
+        #region Clear Old Buttons
+        // Remove all of the previous unit's buttons.
+        foreach (Button unitActionButton in m_SelectedUnitActionButtons)
+        {
+            Destroy(unitActionButton.gameObject);
+        }
+        m_SelectedUnitActionButtons.Clear();
+        #endregion
+
+        // When we will instantiate the button according to the amount of unit actions there are!
+        IUnitAction[] unitActions = _unit.GetComponentsInChildren<IUnitAction>();
+        foreach (IUnitAction unitAction in unitActions)
+        {
+            // Create the new icon using m_UnitActionIcon_Prefab.
+            Image unitActionIconImage = Instantiate(m_UnitActionIconImage_Prefab, m_ScrollRectContent.transform);
+            unitActionIconImage.gameObject.SetActive(true);
+
+            // Replace the icon sprite to be the icon needed for this action.
+            unitActionIconImage.sprite = unitAction.ActionIconUI;
+
             // Add to the onclick event for Unity button!
-            Button zeUnitIconButton = zeUnitIconUI.GetComponent<Button>();
-            zeUnitIconButton.onClick.AddListener(() => ToInstantiateSpecificActionUI(zeUnitAction.UnitActionUI, zeUnitAction));
-            m_AllOfUnitUIIcon.Add(zeUnitIconButton);
+            Button unitActionButton = unitActionIconImage.GetComponent<Button>();
+            unitActionButton.onClick.AddListener(() => ToInstantiateSpecificActionUI(unitAction.UnitActionUI, unitAction));
+
+            // Add this button to m_SelectedUnitActionButtons.
+            m_SelectedUnitActionButtons.Add(unitActionButton);
         }
     }
 }
