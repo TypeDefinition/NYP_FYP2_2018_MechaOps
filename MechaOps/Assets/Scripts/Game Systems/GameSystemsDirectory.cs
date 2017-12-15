bca52ea03d65dbcebaa4588346affe12d2710e82
Terry Lim <lnxterry@gmail.com>
2017-12-15 17:14:59 +0800

    Renamed some more stuff. Added Game Systems Directory. Added ScreenCanvas.cs.

--- MechaOps/Assets/Scripts/Game Systems/GameSystemsDirectory.cs
+++ MechaOps/Assets/Scripts/Game Systems/GameSystemsDirectory.cs
@@ -9,10 +9,14 @@ public class GameSystemsDirectory : MonoBehaviour
     [SerializeField] private Canvas m_ScreenCanvas = null;
     [SerializeField] private Canvas m_WorldCanvas = null;
     [SerializeField] private UnitsTracker m_UnitsTracker = null;
+    [SerializeField] private EnemyUnitManager m_EnemyUnitManager = null;
+    [SerializeField] private PlayerUnitManager m_PlayerUnitManager = null;
 
     public TileSystem GetTileSystem() { return m_TileSystem; }
     public Canvas GetScreenCanvas() { return m_ScreenCanvas; }
     public Canvas GetWorldCanvas() { return m_WorldCanvas; }
     public UnitsTracker GetUnitsTracker() { return m_UnitsTracker; }
+    public EnemyUnitManager GetEnemyUnitManager() { return m_EnemyUnitManager; }
+    public PlayerUnitManager GetPlayerUnitManager() { return m_PlayerUnitManager; }
 
 }
---
+++ MechaOps/Assets/Scripts/UI/ScreenCanvas.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[DisallowMultipleComponent]
+public class ScreenCanvas : MonoBehaviour
+{
+    [SerializeField] private GameObject m_GameWinUI;
+    [SerializeField] private GameObject m_GameLoseUI;
+    [SerializeField] private GameObject m_EnemyActivityUI;
+    [SerializeField] private GameObject m_UnitSelectionUI;
+    [SerializeField] private GameObject m_UnitActionSelectionUI;
+    [SerializeField] private Button m_SettingsButton;
+
+    public GameObject GetGameWinUI() { return m_GameWinUI; }
+    public GameObject GetGameLoseUI() { return m_GameLoseUI; }
+    public GameObject GetEnemyActivityUI() { return m_EnemyActivityUI; }
+    public GameObject GetUnitSelectionUI() { return m_UnitSelectionUI; }
+    public GameObject GetUnitActionSelectionUI() { return m_UnitActionSelectionUI; }
+    public Button GetSettingsButton() { return m_SettingsButton; }
+}
