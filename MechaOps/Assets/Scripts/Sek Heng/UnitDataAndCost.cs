5efca448c4dc5e7b3413f797d39593697d688a84
Ah Heng <leesekheng@yahoo.com>
2017-12-22 17:58:38 +0800

    Moving the file to a different location

---
+++ MechaOps/Assets/Scripts/Sek Heng/UnitDataAndCost.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The container to contain the unit's prefab and the cost to spawn those units!
+/// </summary>
+[CreateAssetMenu(fileName = "UnitDataAndCost", menuName = "Units/SpawnData", order = 1)]
+public class UnitDataAndCost : ScriptableObject {
+
+}
