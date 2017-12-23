4e0a8c1b9258ead7187f15ea928d4a07c7ede9d6
Ah Heng <leesekheng@yahoo.com>
2017-12-23 22:56:53 +0800

    In progress of improving the UI for spawn system in preparation for beta testing

--- MechaOps/Assets/Scripts/Sek Heng/SpawnUnitSystem.cs
+++ MechaOps/Assets/Scripts/Sek Heng/SpawnUnitSystem.cs
@@ -25,6 +25,10 @@ public class SpawnUnitSystem : MonoBehaviour {
     protected GameObject m_SelectUnitUIPrefab;
     [SerializeField, Tooltip("Button prefab for selecting the unit")]
     protected Button m_SelectUnitButtonPrefab;
+    [SerializeField, Tooltip("Name of the the vertical layout group to put in")]
+    protected string m_TagOfLayout = "GroupingImage";
+    [SerializeField, Tooltip("The name of child button of the m_SelectUnitUI")]
+    protected string m_NameOfFinishButton = "Finished";
     [SerializeField, Tooltip("Amount of credit for the player to spend")]
     protected int m_PlayerCredits;
 
@@ -38,14 +42,32 @@ public class SpawnUnitSystem : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+        m_InstantiateSelectUnitUI = Instantiate(m_SelectUnitUIPrefab, m_GameSystem.GetScreenSpaceCanvas().transform);
+        Transform zeVerticalLayoutTransform = GameObject.FindGameObjectWithTag(m_TagOfLayout).transform;
+        foreach (UnitDataAndCost.UnitUI_Data zeUnitUI in m_UnitsDataAsset.UnitUIDataArray)
+        {
+            GameObject zeInstantiateButtonGO = Instantiate(m_SelectUnitButtonPrefab.gameObject, zeVerticalLayoutTransform, false);
+            // and then set the necessary prefabs and values for it!
+            zeInstantiateButtonGO.GetComponent<Image>().sprite = zeUnitUI.m_UnitSpriteUI;
+            zeInstantiateButtonGO.GetComponent<Button>().onClick.AddListener(() =>SetUnitToSpawn(zeUnitUI.m_TypeName));
+        }
+        // Make sure the finished button has the startspawning units function
+        m_InstantiateSelectUnitUI.transform.Find(m_NameOfFinishButton).GetComponent<Button>().onClick.AddListener(StartSpawningUnits);
 	}
 
+    /// <summary>
+    /// When the tile is clicked, it will record down
+    /// </summary>
+    /// <param name="_go"></param>
     void SetUnitPosition(GameObject _go)
     {
 
     }
 
+    /// <summary>
+    /// When the unit UI button is pressed
+    /// </summary>
+    /// <param name="_UnitTypeName">The Typename of the unit</param>
     void SetUnitToSpawn(string _UnitTypeName)
     {
 
