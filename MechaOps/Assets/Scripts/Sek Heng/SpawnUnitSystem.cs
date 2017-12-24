9c16ecce70cdde6bc1a1665f83e263e18e10bcd8
Ah Heng <leesekheng@yahoo.com>
2017-12-24 16:07:05 +0800

    created a script to hold all of the UI for the SpawnSystem is more hardcoded but definitely make life much more simper

--- MechaOps/Assets/Scripts/Sek Heng/SpawnUnitSystem.cs
+++ MechaOps/Assets/Scripts/Sek Heng/SpawnUnitSystem.cs
@@ -25,10 +25,6 @@ public class SpawnUnitSystem : MonoBehaviour {
     protected GameObject m_SelectUnitUIPrefab;
     [SerializeField, Tooltip("Button prefab for selecting the unit")]
     protected Button m_SelectUnitButtonPrefab;
-    [SerializeField, Tooltip("Name of the the vertical layout group to put in")]
-    protected string m_TagOfLayout = "GroupingImage";
-    [SerializeField, Tooltip("The name of child button of the m_SelectUnitUI")]
-    protected string m_NameOfFinishButton = "Finished";
     [SerializeField, Tooltip("Amount of credit for the player to spend")]
     protected int m_PlayerCredits;
 
@@ -37,22 +33,28 @@ public class SpawnUnitSystem : MonoBehaviour {
     protected string m_CurrentSelectedUnitName;
     [SerializeField, Tooltip("Reference to the instantiated prefab")]
     protected GameObject m_InstantiateSelectUnitUI;
+    [SerializeField, Tooltip("Script that handles the spawn UI logic. Should be attached to m_InstantiateSelectUnitUI")]
+    protected SpawnUnitUI_Logic m_SpawnUI_Logic;
     [SerializeField, Tooltip("List of player units that will be spawn later")]
     protected List<UnitSpawnData> m_ListOfSpawnUnits = new List<UnitSpawnData>();
 
 	// Use this for initialization
 	void Start () {
         m_InstantiateSelectUnitUI = Instantiate(m_SelectUnitUIPrefab, m_GameSystem.GetScreenSpaceCanvas().transform);
-        Transform zeVerticalLayoutTransform = GameObject.FindGameObjectWithTag(m_TagOfLayout).transform;
+        // Need to make sure it will always be active
+        m_InstantiateSelectUnitUI.SetActive(true);
+        m_SpawnUI_Logic = m_InstantiateSelectUnitUI.GetComponent<SpawnUnitUI_Logic>();
+        Transform zeVerticalLayoutTransform = m_SpawnUI_Logic.UnitLayoutUI.transform;
+        // iterate through the unit UI array and spawn the UI buttons
         foreach (UnitDataAndCost.UnitUI_Data zeUnitUI in m_UnitsDataAsset.UnitUIDataArray)
         {
             GameObject zeInstantiateButtonGO = Instantiate(m_SelectUnitButtonPrefab.gameObject, zeVerticalLayoutTransform, false);
             // and then set the necessary prefabs and values for it!
             zeInstantiateButtonGO.GetComponent<Image>().sprite = zeUnitUI.m_UnitSpriteUI;
-            zeInstantiateButtonGO.GetComponent<Button>().onClick.AddListener(() =>SetUnitToSpawn(zeUnitUI.m_TypeName));
+            zeInstantiateButtonGO.GetComponent<Button>().onClick.AddListener(() =>SetUnitToSpawn(zeUnitUI.m_TypeName, zeUnitUI.m_UnitSpriteUI));
         }
         // Make sure the finished button has the startspawning units function
-        m_InstantiateSelectUnitUI.transform.Find(m_NameOfFinishButton).GetComponent<Button>().onClick.AddListener(StartSpawningUnits);
+        m_SpawnUI_Logic.FinishedButton.onClick.AddListener(StartSpawningUnits);
 	}
 
     /// <summary>
@@ -68,9 +70,12 @@ public class SpawnUnitSystem : MonoBehaviour {
     /// When the unit UI button is pressed
     /// </summary>
     /// <param name="_UnitTypeName">The Typename of the unit</param>
-    void SetUnitToSpawn(string _UnitTypeName)
+    /// <param name="_UnitUI_Sprite">Unit UI Sprite</param>
+    void SetUnitToSpawn(string _UnitTypeName, Sprite _UnitUI_Sprite)
     {
-
+        m_CurrentSelectedUnitName = _UnitTypeName;
+        m_SpawnUI_Logic.UnitTypenameText = _UnitTypeName;
+        m_SpawnUI_Logic.UnitUI_ImageSprite = _UnitUI_Sprite;
     }
 
     /// <summary>
@@ -80,5 +85,13 @@ public class SpawnUnitSystem : MonoBehaviour {
     {
         // destroy the gameobject that this is attached to when it is done
         Destroy(gameObject);
+    }
+
+    /// <summary>
+    /// When this component is destroyed, destroy the UI too!
+    /// </summary>
+    private void OnDestroy()
+    {
+        Destroy(m_InstantiateSelectUnitUI);
     }
 }
---
+++ MechaOps/Assets/Scripts/Sek Heng/SpawnUnitUI_Logic.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+/// <summary>
+/// Set the current selected image and name to the player selected unit!
+/// </summary>
+public class SpawnUnitUI_Logic : MonoBehaviour {
+    [Header("Variable for SpawnUnitUI_Logic")]
+    [SerializeField, Tooltip("Text UI of the unit type name")]
+    protected TextMeshProUGUI m_TMProTextUI;
+    [SerializeField, Tooltip("Image of the unit")]
+    protected Image m_UnitUI_Image;
+    [SerializeField, Tooltip("Layout group of the unit's UI")]
+    protected VerticalLayoutGroup m_UnitLayoutUI;
+    [SerializeField, Tooltip("Finished button")]
+    protected Button m_FinishedButton;
+
+    public Sprite UnitUI_ImageSprite
+    {
+        set
+        {
+            // need to ensure that the image color is normal
+            if (m_UnitUI_Image.color.a < 255)
+            {
+                // change the alpha to be 255
+                m_UnitUI_Image.color = new Color(m_UnitUI_Image.color.r, m_UnitUI_Image.color.g, m_UnitUI_Image.color.b, 255);
+            }
+            m_UnitUI_Image.sprite = value;
+        }
+        get
+        {
+            return m_UnitUI_Image.sprite;
+        }
+    }
+
+    public string UnitTypenameText
+    {
+        set
+        {
+            m_TMProTextUI.text = value;
+        }
+        get
+        {
+            return m_TMProTextUI.text;
+        }
+    }
+
+    public VerticalLayoutGroup UnitLayoutUI
+    {
+        get
+        {
+            return m_UnitLayoutUI;
+        }
+    }
+
+    public Button FinishedButton
+    {
+        get
+        {
+            return m_FinishedButton;
+        }
+    }
+}
