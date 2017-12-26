846edae0aea2dbf8f5ce616090e9e586e3e8efc7
Terry Lim <lnxterry@gmail.com>
2017-12-27 03:36:21 +0800

    Completed MainMenu Start Screen & Level Selection.

---
+++ MechaOps/Assets/Scripts/MainMenu/LevelSelectionLibrary.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class LevelSelectionData
+{
+    [SerializeField] private string m_SceneName = null;
+
+    [SerializeField] private string m_LevelName = null;
+    [SerializeField] private string m_LevelDescription = null;
+    [SerializeField] private Sprite m_LevelIcon = null;
+
+    public string GetSceneName() { return m_SceneName; }
+
+    public string GetLevelName() { return m_LevelName; }
+    public string GetLevelDescription() { return m_LevelDescription; }
+    public Sprite GetLevelIcon() { return m_LevelIcon; }
+}
+
+[CreateAssetMenu]
+public class LevelSelectionLibrary : ScriptableObject
+{
+    [SerializeField] private LevelSelectionData[] m_Levels = null;
+
+    public LevelSelectionData[] GetLevelSelectionData() { return m_Levels; }
+}
---
+++ MechaOps/Assets/Scripts/MainMenu/MenuStateChangeButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class MenuStateChangeButton : MonoBehaviour
+{
+    [SerializeField] private MainMenuManager m_MainMenuManager = null;
+    [SerializeField] private MainMenuManager.MenuState m_MenuState = MainMenuManager.MenuState.StartScreen;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(m_MainMenuManager, MethodBase.GetCurrentMethod().Name + " - m_MainMenuManager must not be null!");
+    }
+
+    public void OnClick()
+    {
+        m_MainMenuManager.SetMenuState(m_MenuState);
+    }
+}
