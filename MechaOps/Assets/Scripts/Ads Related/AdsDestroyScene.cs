10eac442794563defb891b593b71b28e45d9a748
Ah Heng <leesekheng@yahoo.com>
2018-01-20 22:07:15 +0800

    Finished Integration of unity ads into the game

---
+++ MechaOps/Assets/Scripts/Ads Related/AdsDestroyScene.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class AdsDestroyScene : MonoBehaviour {
+    [Header("Debugging for AdsDestroyScene")]
+    [SerializeField, Tooltip("Script of HandleAdScript")]
+    protected HandleAdsScript m_HandleAds;
+    [SerializeField, Tooltip("Name of the scene to unload when ads is completed")]
+    protected string m_SceneNameToUnload = "Ads_Menu";
+
+    private void OnEnable()
+    {
+        if (!m_HandleAds)
+        {
+            m_HandleAds = GetComponent<HandleAdsScript>();
+        }
+        m_HandleAds.ShowOpt.resultCallback += CompleteAds;
+    }
+
+    private void OnDisable()
+    {
+        m_HandleAds.ShowOpt.resultCallback -= CompleteAds;
+    }
+
+    private void CompleteAds(ShowResult _result)
+    {
+        // regardless of the result just unload the scene
+        Destroy(gameObject);
+    }
+
+    private void OnDestroy()
+    {
+        SceneHelperSingleton.Instance.UnloadScene(m_SceneNameToUnload);
+    }
+}
---
+++ MechaOps/Assets/Scripts/Ads Related/HandleAdsScript.cs
@@ -0,0 +1,52 @@
+﻿//#define LIVE_BUILD
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class HandleAdsScript : MonoBehaviour {
+    [Header("Variables for HandleAdsScript")]
+    [SerializeField, Tooltip("Uncheck this if this build is going live")]
+    protected bool m_TestBuild = true;
+    [SerializeField, Tooltip("Placement ID of the video which can be seen at the unity operate sites")]
+    protected string m_PlacementID = "video";
+
+    protected ShowOptions m_ShowOption;
+    public ShowOptions ShowOpt
+    {
+        get { return m_ShowOption; }
+    }
+
+#if UNITY_ANDROID || UNITY_EDITOR
+    const string m_AdsGameID = "1676230";
+#elif UNITY_IOS
+    const string m_AdsGameID = "1676229";
+#endif
+
+    // Use this for initialization
+    void Awake () {
+        // if there is no internet connection at all, dont display the ads at all!
+        if (Application.internetReachability == NetworkReachability.NotReachable)
+        {
+            Destroy(gameObject);
+            return;
+        }
+#if LIVE_BUILD
+        m_TestBuild = false;
+#endif
+        Advertisement.Initialize(m_AdsGameID, m_TestBuild);
+        m_ShowOption = new ShowOptions();
+    }
+
+    IEnumerator Start()
+    {
+        // Have to loop until it is ready!
+        while (!Advertisement.IsReady(m_PlacementID))
+        {
+            yield return null;
+        }
+        Advertisement.Show(m_PlacementID, m_ShowOption);
+        yield break;
+    }
+}
